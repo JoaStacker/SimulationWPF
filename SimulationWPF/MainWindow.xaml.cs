1cdf59f183e6d5715991c966f8641f736221d70c
JoaStacker <joessar99@gmail.com>
2024-05-06 22:21:24 -0300

    Add project files.

---
+++ SimulationWPF/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimulationWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowsFormsHost_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+            //System.Windows.MessageBox.Show("Button clicked!");
+        }
+
+        private void Button_Click_1(object sender, EventArgs e)
+        {
+            Parameters parameters = new Parameters();
+            parameters.Show();
+        }
+    }
+}
